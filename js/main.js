8c55f7a53b107ab0da11f5d15178d7ce0eedbd3d
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-09-29 11:20:08 -0400

    main section mobile friednly

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(function () {
     nextArrow:
       '<button type="button" class="slick-next"><img class="next" src="/images/next.svg" alt="" /></button>',
     vertical: true,
+    fade: true,
   });
 
   $(".product__name").slick({
@@ -18,6 +19,24 @@ $(function () {
       '<button type="button" class="product-prev"><img class="prev" src="/images/top.svg" alt=""></button>',
     nextArrow:
       '<button type="button" class="product-next"><img class="next" src="/images/button.svg" alt="" /></button>',
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          vertical: false,
+          slidesToShow: 3,
+          arrows: false,
+          centerMode: true,
+        },
+        breakpoint: 600,
+        settings: {
+          vertical: false,
+          slidesToShow: 1,
+          arrows: false,
+          centerMode: true,
+        },
+      },
+    ],
   });
   $(".product__content").slick({
     asNavFor: ".product__name",
